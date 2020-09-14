014290757b42bc7745855a942c1c87bfb312c7bb
romina moreno <romina.moreno@fing.edu.uy>
2020-09-14 03:51:03 -0300

    Assignment 2

---
+++ Curso2/React/confusion/SPA.md
@@ -0,0 +1,6 @@
+# Single Page Applications
+
+* [Single Page Applications (Wikipedia)](https://en.wikipedia.org/wiki/Single-page_application)
+* [Deep linking](https://en.wikipedia.org/wiki/Deep_linking)
+* [Single Page Apps in depth](http://singlepageappbook.com/)
+* [SPA and the Single Page Myth](http://www.johnpapa.net/pageinspa/)
--- Curso2/React/confusion/src/components/MainComponent.js
+++ Curso2/React/confusion/src/components/MainComponent.js
@@ -5,6 +5,7 @@ import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
 import { LEADERS } from "../shared/leaders";
@@ -48,6 +49,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
